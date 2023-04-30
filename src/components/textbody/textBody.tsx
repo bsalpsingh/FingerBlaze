6ad6d7b3371d80110824db49271d985abbebef2b
bsalpsingh <bishal.singh@berrybytes.com>
2023-04-30 12:46:01 +0545

    added backwards functionality

--- src/components/textbody/textBody.tsx
+++ src/components/textbody/textBody.tsx
@@ -5,17 +5,17 @@ interface Props {
   tabIndex?: string;
 }
 
-const phrase: string =
+const phraseString: string =
   "Maycomb was an old town, but it was a tired old town when I first knew it. In rainy weather the streets turned to red slop; grass grew on the sidewalks, the courthouse sagged in the square. Somehow, it was hotter then: a black dog suffered on a summer's day; bony mules hitched to Hoover carts flicked flies in the sweltering shade of the live oaks on the square. Men's stiff collars wilted by nine in the morning";
 export const TextBody = (props: Props) => {
   const inputRef = useRef<HTMLInputElement>(null);
-  // const [phrase, setPhrase] = React.useState<string>(phraseString);
+  const [phrase, setPhrase] = React.useState<string>(phraseString);
   const [inputPhrase, setInputPhrase] = React.useState<string>("");
   const [phraseIndex, setPhraseIndex] = React.useState<number>(0);
   const [typo, setTypo] = React.useState<boolean>(false);
   const [startTime, setStartTime] = useState<Date | null>(null);
   const [endTime, setEndTime] = useState<Date | null>(null);
-
+  const allowedKeys = /[a-zA-Z0-9.!?]/;
   React.useEffect(() => {
     if (inputRef.current) {
       inputRef.current.focus();
@@ -26,17 +26,43 @@ export const TextBody = (props: Props) => {
     if (phraseIndex === 0 && !startTime) {
       setStartTime(new Date());
     }
+    setEndTime(new Date());
+
     if (phrase[phraseIndex] === event.key) {
       setPhraseIndex(phraseIndex + 1);
-      if (phraseIndex + 1 === phrase.length - 1) {
-        setEndTime(new Date());
-      }
+      // if (phraseIndex === phrase.length - 1) {
+      //   setEndTime(new Date());
+      // }
       setTypo(false);
       setInputPhrase((phrase: string) => {
         phrase = phrase + event.key;
         return phrase;
       });
+    } else if (event.code === "Backspace") {
+      if (phraseIndex === 0) {
+        setTypo(false);
+        return;
+      }
+      if (typo) {
+        setTypo(false);
+      } else {
+        setPhraseIndex(phraseIndex - 1);
+        setInputPhrase((phrase: string) => {
+          phrase = phrase.substring(0, phraseIndex - 1);
+
+          return phrase;
+        });
+      }
     } else {
+      if (
+        !allowedKeys.test(event.key) ||
+       [' ','.',',',":",";"].includes (phrase[phraseIndex] )
+       
+      ) {
+        setTypo(false);
+        return;
+      }
+
       setTypo(true);
     }
   };
@@ -50,17 +76,19 @@ export const TextBody = (props: Props) => {
     let diffInSeconds = Math.floor((date2.getTime() - date1.getTime()) / 1000);
     return phraseIndex / diffInSeconds;
   };
-  console.log("input pharse", inputPhrase);
+  console.log("input pharse", inputPhrase, "\n", phrase);
 
   return (
     <>
-      {phraseIndex === phrase.length && (
+      {!!phraseIndex && (
         <div className="text-4xl text-red-600 text-center">
           {" "}
           Speed : {getRate()?.toFixed(0) || "-"} CPM ~{" "}
           {Math.ceil(((getRate() || 0) / 5.5) * 60)} WPM
         </div>
       )}{" "}
+      {/* <span>{inputPhrase}</span>
+      <span>{phrase}</span> */}
       <div
         onKeyDown={handleKeyDown}
         className="container mx-auto mt-8 p-8  h-auto border-2 border-rose-400 rounded-4xl "
@@ -70,8 +98,8 @@ export const TextBody = (props: Props) => {
           <span className="text-4xl inline">
             <span className=" text-red-500">{inputPhrase}</span>
             <span
-              className={`text-slate-500 underline ${
-                typo ? "line-through bg-red-200" : ""
+              className={`text-slate-500 bg-red-200 ${
+                typo ? "line-through " : ""
               }`}
             >
               {phrase[phraseIndex]}
